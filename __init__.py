9821037720b4db6c46395266a6e209c0fbd5fb46
avnerdorman <avnerdorman@gmail.com>
2020-01-02 19:58:37 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-}
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 19:58:25 2020
+
+@author: adorman
+"""
+
